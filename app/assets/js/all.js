db7a73e9fcb17d7b429109dd4007ef8588efd1ad
Shawn <ttony3062002@gmail.com>
2021-08-26 15:03:41 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,12 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+var courseSwiper = new Swiper(".course-swiper ", {
+  slidesPerView: "1.5",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
