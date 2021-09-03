916b823177ddfd23e51245afd5c7e2c6453fd3c9
Shawn <ttony3062002@gmail.com>
2021-09-03 13:48:49 +0800

    剩下的頁面

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,7 +13,7 @@ var courseSwiper = new Swiper(".course-swiper ", {
 var customerSwiper = new Swiper(".customer-swiper", {
   slidesPerView: 2,
   grid: {
-    rows: 2,
+    rows: 1,
   },
   spaceBetween: 30,
   pagination: {
@@ -22,3 +22,32 @@ var customerSwiper = new Swiper(".customer-swiper", {
   },
 });
 
+var teacherSwiper = new Swiper(".teacher-swiper", {
+  slidesPerView: "2",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var courseRecommendSwiper = new Swiper(".course-recommend-swiper", {
+  slidesPerView: "1.5",
+  spaceBetween: 30,
+  breakpoints: { 
+   //當寬度小於等於480
+    768: { 
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    //當寬度小於等於640
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
