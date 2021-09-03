e5635a693a9acbaa70b773494f173535e351b3ea
Shawn <ttony3062002@gmail.com>
2021-09-03 13:58:20 +0800

    Update 2021-09-03T05:58:19.043Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -14,9 +14,37 @@ var courseSwiper = new Swiper(".course-swiper ", {
 var customerSwiper = new Swiper(".customer-swiper", {
   slidesPerView: 2,
   grid: {
-    rows: 2
+    rows: 1
   },
   spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+var teacherSwiper = new Swiper(".teacher-swiper", {
+  slidesPerView: "2",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+var courseRecommendSwiper = new Swiper(".course-recommend-swiper", {
+  slidesPerView: "1.5",
+  spaceBetween: 30,
+  breakpoints: {
+    //當寬度小於等於480
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    //當寬度小於等於640
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true
